fb5883a704867175ce05cd0861539a83c6f2eba9
RokuNine <94667767+RokuNine@users.noreply.github.com>
2021-11-19 13:25:15 -0800

    Update server.py

--- server.py
+++ server.py
@@ -7,10 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template('./index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
